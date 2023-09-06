e525185bdc3a00a660ef4ad539e5eb92fe661431
徳田 真之介 <tokuda_shinnosuke@cyberagent.co.jp>
2023-09-06 17:46:31 +0900

    add: cloud sql

---
+++ terraform/modules/cloud-sql/cloud-sql.tf
@@ -0,0 +1,23 @@
+resource "google_sql_database_instance" "db" {
+  name                = "${var.environment}-${var.project}-db-instance"
+  database_version    = "MYSQL_8_0"
+  region              = var.region
+  deletion_protection = false 
+
+  settings {
+    tier              = "db-f1-micro"
+    availability_type = "REGIONAL"
+    disk_size         = "20"
+    disk_type         = "PD_SSD"
+
+    ip_configuration {
+        private_network = var.vpc_name
+      ipv4_enabled = "true"
+    }
+  }
+}
+
+resource "google_sql_database" "db" {
+  name     = "${var.environment}-${var.project}-db"
+  instance = google_sql_database_instance.db.name
+}
---
+++ terraform/modules/cloud-sql/variable.tf
@@ -0,0 +1,6 @@
+variable "project_id" {}
+variable "region" {}
+variable "environment" {}
+variable "project" {}
+
+variable "vpc_name" {}
