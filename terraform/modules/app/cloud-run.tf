a27f1344e1e9c5c6e6805b0eb6f148943ddd658c
徳田 真之介 <tokuda_shinnosuke@cyberagent.co.jp>
2024-02-15 21:13:30 +0900

    fix: container image

--- terraform/modules/app/cloud-run.tf
+++ terraform/modules/app/cloud-run.tf
@@ -37,7 +37,6 @@ resource "google_cloud_run_v2_service" "app" {
       client,
       client_version,
       template[0].containers[0].image,
-      template[0].containers[0].volume_mounts,
     ]
   }
 }
